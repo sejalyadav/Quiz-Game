1781f23bad43ed60d1a1cb133a7c72e1a47a5e3f
sejalyadav <82174732+sejalyadav@users.noreply.github.com>
2022-05-24 09:16:57 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,118 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A pair of two terminal electrical network in which current enters through one terminal and leaves through another terminal of each port is called as.(Remember)",
+    answer: "Two port network",
+    options: [
+      "Multiple port network",
+      "Two port network",
+      "Single port network",
+      "None of the above"
+    ], 
+  },
+    {
+    numb: 2,
+    question: "A network consisting of three impedance branches connected in star is called as ____ network. (Remember)",
+    answer: "T-network",
+    options: [
+      "Hybrid network",
+      "Pi-network",
+      "T-network",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The relation between input and output signals of the network can be determined by transferring various network parameters, such as?(Understand) ",
+    answer: "Impedance, admittance, voltage ratio and current ratio",
+    options: [
+      "Input impedance",
+      "voltage ratio",
+      "resistance",
+      "Impedance, admittance, voltage ratio and current ratio"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A 2-port resistive network satisfy the condition A = D = 3/2B = 4/3C. The z11 of the network is? (Understand)",
+    answer: "4/3",
+    options: [
+      "4/3",
+      "3/4",
+      "2/3",
+      "3/2"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The condition AD - BC = 1 for a two-port network implies that the network is a? (Apply)",
+    answer: "Reciprocal network",
+    options: [
+      "Lossless network",
+      "Lumped element network",
+      "Reciprocal network",
+      "Unilateral element network"
+    ]
+  },
+
+    {
+    numb: 6,
+    question: "In the circuit given below, the value of the hybrid parameter h21 is (Apply) <img src ='app image.jpg' height=100 width=200>",
+    answer: "0.5ohm",
+    options: [
+      "10ohm",
+      "0.5ohm",
+      "5ohm",
+      "2.5ohm"
+     ]
+   },
+
+   {
+    numb: 7,
+    question: "Obtain Y-parameters of the Two-port network shown (Analyze) <img src ='ana 1.png' height=100 width=200>",
+    answer: "20/7, (-5)/7, (-5)/7, 26/7",
+    options: [
+      "26/7, (-5)/7, (-5)/7, 20/7",
+      "(-5)/7, (-5)/7, 20/7, 26/7",
+      "20/7, (-5)/7, (-5)/7, 26/7",
+      "None of the above"
+     ]
+   },
+
+   {
+    numb: 8,
+    question: "For a two-port, let A = 4, B = 30Ω, C = 0.1 S, and D = 1.5. The input impedance Zin = V1 /I1 when the output terminals are short-circuited will be, (Analyze)",
+    answer: "30Ω",
+    options: [
+      "0.87Ω",
+      "4.5Ω",
+      "16Ω",
+      "30Ω"
+     ]
+   },
+
+   {
+    numb: 9,
+    question: "Determine the hybrid parameters for the network. (Evaluate) <img src ='eval.png' height=100 width=200>",
+    answer: "[h] = 38Ω, 0.4, -3.6, 0.2S",
+    options: [
+      "[h] = 43, 0.4, -3.6, 0.2S",
+      "[h] = 38Ω, 0, -3.6, 0.2S",
+      "[h] = 38Ω, 0.4, -3.6, 0.2S",
+      "[h] = 38Ω, 0.4, -3.6, 0.87"
+     ]
+   },
+
+   {
+    numb: 10,
+    question: "For the circuit shown below let [z] = (10, -6, -4, 12). Find I1 , I2 ,V1, and V2. (Create) <img src ='create1.png' height=100 width=300>",
+    answer: "I1= 0.8049 A, I2= 0.1463 A, V1= 7.1706 and V2= -1.463 V",
+    options: [
+      "I1= 0.8049 A, I2= 0.1463 A, V1= 7.1706 and V2= -1.463 V",
+      "I1= 0.8049 A, I2= 2.1463 A, V1= 5.6706 and V2= -1.463 V",
+      "I1= 0.1436 A, I2= 4.1567 A, V1= 7.1706 and V2= -1.463 V",
+      "I1= 0.1436A, I2= 6.1706 A, V1= 4.5670and V2= -1.463 V"
+     ]
+   },
+];
